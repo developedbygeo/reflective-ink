3098371fe146c18d28e372b3a5a33c947d2158c0
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-11 00:48:08 +0200

    feat: signup page

---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,20 @@
+import { SignUp } from '@clerk/nextjs';
+
+import MovingGradient from '@/components/UI/MovingGradient';
+import { authDialogueStyle } from '@/common/authStyling';
+
+const SignupPage = () => {
+  return (
+    <section className="flex items-center w-screen h-screen justify-center bg-pur">
+      <MovingGradient className="pointer-events-none">
+        <SignUp
+          afterSignUpUrl="/new-user"
+          redirectUrl="/new-user"
+          appearance={authDialogueStyle}
+        />
+      </MovingGradient>
+    </section>
+  );
+};
+
+export default SignupPage;
