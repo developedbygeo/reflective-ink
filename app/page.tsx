a09cdb81423a4993ac1342dd3a2322860b147fcf
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-11 00:45:55 +0200

    fix: gradient no pointer-events

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 import { Button } from '@/components/UI/Button';
 import MovingGradient from '@/components/UI/MovingGradient';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="w-screen h-screen bg-gray-100 dark:bg-darkGray flex justify-center items-center text-white relative">
-      <MovingGradient />
+      <MovingGradient className="pointer-events-none" />
       <article className="w-full max-w-[40rem] mx-auto">
         <h1 className="text-4xl w-2/3 mb-4">Welcome to Reflective Ink.</h1>
         <p className="text-xl font-light h-4/5 text-gray-300 mb-6">
           An AI-powered app for tracking your mood. Interested in learning more?
         </p>
-        <Button size="xl">Get Started</Button>
+        <Button size="xl" asChild>
+          <Link href="/my-journal">Get Started</Link>
+        </Button>
       </article>
     </div>
   );
