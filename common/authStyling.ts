1488c877fc0e319e2c5b7401fd49653fdd3aab1d
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-11 00:48:21 +0200

    style: auth dialogue styling

---
+++ common/authStyling.ts
@@ -0,0 +1,9 @@
+export const authDialogueStyle = {
+  elements: {
+    card: 'font-sans bg-gray-100',
+    headerTitle: 'text-purple-800 font-display',
+    headerSubtitle: 'font-sans font-light',
+    socialButtonsBlockButton: 'font-sans',
+    formButtonPrimary: 'bg-purple-500 hover:bg-purple-600 font-sans',
+  },
+};
