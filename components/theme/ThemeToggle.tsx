76b5aee10a1a404330c4360db9cedbe7988965f9
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-12 01:49:05 +0200

    style: updating colors

--- components/theme/ThemeToggle.tsx
+++ components/theme/ThemeToggle.tsx
@@ -12,15 +12,15 @@ import {
   DropdownMenuTrigger,
 } from '@/components/UI/DropdownMenu';
 
-export function ModeToggle() {
+const ThemeToggle = () => {
   const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <LuSun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <LuMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button className="group" variant="outline" size="icon">
+          <LuSun className="h-6 w-6 rotate-0 scale-100 text-white group-hover:text-darkGray transition-all dark:-rotate-90 dark:scale-0" />
+          <LuMoon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
@@ -37,4 +37,6 @@ export function ModeToggle() {
       </DropdownMenuContent>
     </DropdownMenu>
   );
-}
+};
+
+export default ThemeToggle;
