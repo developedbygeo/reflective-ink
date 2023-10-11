ed37f89c5d51f104e89a3552eaee0aceb534d8f6
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-12 01:48:10 +0200

    feat: my-journal page

---
+++ app/(dashboard)/my-journal/page.tsx
@@ -0,0 +1,27 @@
+import EntryCard from '@/modules/myJournal/EntryCard'
+import NewEntryCard from '@/modules/myJournal/NewEntryCard'
+import { findUserByClerkId } from '@/utils/auth'
+import { getJournalEntries } from '@/utils/prisma-utils'
+
+const getEntries = async () => {
+  const user = await findUserByClerkId()
+  const entries = getJournalEntries(user.id, 'desc')
+  return entries
+}
+
+const MyJournalPage = async () => {
+  const entries = await getEntries()
+  return (
+    <div className="max-w-6xl mx-auto mt-20">
+      <h2 className="mb-8 text-2xl">My Journal</h2>
+      <section className="grid grid-cols-3 gap-4 mt-8">
+        <NewEntryCard />
+        {entries.map((entry) => (
+          <EntryCard entry={entry} key={entry.id} />
+        ))}
+      </section>
+    </div>
+  )
+}
+
+export default MyJournalPage
