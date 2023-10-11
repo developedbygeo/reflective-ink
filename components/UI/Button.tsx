726599bfb86d444bc853ba5702f15496805a18b9
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-12 01:49:14 +0200

    style: updating outline

--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -1,11 +1,11 @@
-'use client';
+'use client'
 
-import { forwardRef } from 'react';
-import { Slot } from '@radix-ui/react-slot';
+import { forwardRef } from 'react'
+import { Slot } from '@radix-ui/react-slot'
 
-import { cva, type VariantProps } from 'class-variance-authority';
+import { cva, type VariantProps } from 'class-variance-authority'
 
-import classnameJoin from '@/utils/ui';
+import classnameJoin from '@/utils/ui'
 
 const buttonVariants = cva(
   'inline-flex items-center active:scale-95 justify-center rounded-md text-sm font-medium transition-[transform_colors] duration-200 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-stone-950 disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-stone-300 font-display cursor-pointer',
@@ -17,7 +17,7 @@ const buttonVariants = cva(
         destructive:
           'bg-red-500 text-stone-50 shadow-sm hover:bg-red-500/90 dark:bg-red-900 dark:text-stone-50 dark:hover:bg-red-900/90',
         outline:
-          'border border-stone-200 bg-transparent shadow-sm hover:bg-stone-100 hover:text-stone-900 dark:border-stone-800 dark:hover:bg-stone-800 dark:hover:text-stone-50',
+          'border border-stone-200 bg-transparent shadow-sm hover:bg-stone-100 hover:text-stone-900 dark:border-stone-800 dark:hover:bg-stone-800/10 dark:hover:text-stone-50',
         secondary:
           'bg-stone-100 text-stone-900 shadow-sm hover:bg-stone-100/80 dark:bg-stone-800 dark:text-stone-50 dark:hover:bg-stone-800/80',
         ghost:
@@ -29,7 +29,7 @@ const buttonVariants = cva(
         sm: 'h-8 rounded-md px-3 text-xs',
         lg: 'h-10 rounded-md px-8',
         xl: 'h-14 rounded-md px-10 py-4 text-base',
-        icon: 'h-9 w-9',
+        icon: 'h-10 w-10',
       },
     },
     defaultVariants: {
@@ -37,26 +37,26 @@ const buttonVariants = cva(
       size: 'default',
     },
   },
-);
+)
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean;
+  asChild?: boolean
 }
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : 'button';
+    const Comp = asChild ? Slot : 'button'
     return (
       <Comp
         className={classnameJoin(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
-    );
+    )
   },
-);
-Button.displayName = 'Button';
+)
+Button.displayName = 'Button'
 
-export { Button, buttonVariants };
+export { Button, buttonVariants }
