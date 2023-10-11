a6e73741b26c02e89fd9a3d276f6d02084a09346
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-12 01:48:53 +0200

    chore: renaming

--- common/authStyling.ts
+++
@@ -1,9 +0,0 @@
-export const authDialogueStyle = {
-  elements: {
-    card: 'font-sans bg-gray-100',
-    headerTitle: 'text-purple-800 font-display',
-    headerSubtitle: 'font-sans font-light',
-    socialButtonsBlockButton: 'font-sans',
-    formButtonPrimary: 'bg-purple-500 hover:bg-purple-600 font-sans',
-  },
-};
---
+++ common/clerkStyling.ts
@@ -0,0 +1,22 @@
+export const authDialogueStyle = {
+  elements: {
+    card: 'font-sans bg-gray-100',
+    headerTitle: 'text-purple-800 font-display',
+    headerSubtitle: 'font-sans font-light',
+    socialButtonsBlockButton: 'font-sans',
+    formButtonPrimary: 'bg-purple-500 hover:bg-purple-600 font-sans',
+  },
+};
+
+export const userButtonStyle = {
+  elements: {
+    userButtonPopoverCard: 'bg-gray-200',
+    userButtonPopoverActions: 'mt-2',
+    avatarBox: 'w-12 h-12',
+    userButtonPopoverActionButton: 'hover:bg-purple-500/10 py-3',
+    userButtonPopoverActionButtonText: 'text-xs',
+    userButtonPopoverActionButtonIcon: 'w-5 h-5',
+    userPreviewMainIdentifier: 'text-xs font-sans',
+    userPreviewSecondaryIdentifier: 'text-xs text-purple-800 font-sans',
+  },
+};
