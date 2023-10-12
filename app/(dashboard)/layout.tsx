996446afc48ab5cac375b969cefbc313c28cea8e
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-13 01:58:49 +0200

    style: wrapper layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,7 +5,7 @@ import { WithChildren } from '@/types/UI';
 import { UserButton } from '@clerk/nextjs';
 
 const Layout = ({ children }: WithChildren) => (
-  <div className="h-screen w-screen relative bg-darkGray">
+  <div className="min-h-[100vh] overflow-y-auto relative bg-darkGray">
     <header className="h-16 border-b flex items-center justify-end px-8 py-4 border-darkGray">
       <div className="flex gap-6">
         <ThemeToggle />
