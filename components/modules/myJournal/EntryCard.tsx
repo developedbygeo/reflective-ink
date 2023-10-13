7dc5ca62cf94c345dc86a7016401dc56cdb420b4
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-13 02:00:01 +0200

    refactor: updating entryCard layout, styling and data

--- components/modules/myJournal/EntryCard.tsx
+++ components/modules/myJournal/EntryCard.tsx
@@ -1,30 +1,47 @@
+import { Entry } from '@prisma/client';
 import {
   Card,
+  CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
 } from '@/components/UI/Card';
-import { Entry } from '@prisma/client';
+import { CommonProps } from '@/types/UI';
+import classnameJoin from '@/utils/ui';
 
-type EntryCardProps = {
-  entry: Entry;
+type EntryCardAnimation = {
+  delay: string;
+  duration: string;
 };
 
-const EntryCard = ({ entry }: EntryCardProps) => {
-  const parsedDate = new Date(entry.createdAt).toLocaleDateString('en-GB', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
+type EntryCardProps = CommonProps & {
+  entry: Entry;
+  animation: EntryCardAnimation;
+};
+
+const EntryCard = ({ className, animation, entry }: EntryCardProps) => {
+  const parsedCreatedAt = new Date(entry.createdAt).toDateString();
   return (
-    <div className="relative">
-      <div className="absolute inset-0 -bottom-2 h-full w-full bg-gradient-to-r from-red-300 to-blue-500 transform scale-[0.50] bg-red-500 rounded-full blur-2xl z-0" />
+    <div className={classnameJoin('relative', className)}>
+      <div
+        className="absolute inset-0 -bottom-2 animate-pulse delay-300 h-full w-full bg-gradient-to-r from-red-100 to-blue-500 transform scale-[0.8] bg-red-500 rounded-full blur-[2.25rem] z-0"
+        style={{
+          animationDelay: animation.delay,
+          animationDuration: animation.duration,
+        }}
+      />
       <Card className="shadow-neumorphicTopLeft relative z-20">
-        <CardDescription>Entry</CardDescription>
+        <CardContent className="px-5 py-4 space-y-4 text-textLight">
+          <CardDescription className="mt-3 text-textLight">
+            {entry.content}
+          </CardDescription>
+          <CardDescription className="text-textLight">summary</CardDescription>
+          <CardDescription className="text-textLight">mood</CardDescription>
+        </CardContent>
         <CardFooter>
           <p>
             Created at:
-            <span>22/02/2023</span>
+            <span>{parsedCreatedAt}</span>
           </p>
         </CardFooter>
       </Card>
