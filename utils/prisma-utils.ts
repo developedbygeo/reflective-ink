dff3d04391280162a5c6d4a80efde45f8f0a1bbe
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-13 02:01:06 +0200

    chore: adding helper for unique entry

--- utils/prisma-utils.ts
+++ utils/prisma-utils.ts
@@ -1,4 +1,5 @@
 import { prisma } from '@/utils/db';
+import { findUserByClerkId } from '@/utils/auth';
 
 export const getJournalEntries = async (
   userId: string,
@@ -11,7 +12,44 @@ export const getJournalEntries = async (
     orderBy: {
       createdAt,
     },
+    include: {
+      analysis: true,
+    },
   });
 
   return entries;
+};
+
+export const getJournalEntry = async (id: string) => {
+  const user = await findUserByClerkId();
+  const entry = await prisma.entry.findUnique({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id,
+      },
+    },
+    include: {
+      analysis: true,
+    },
+  });
+
+  return entry;
+};
+
+export const updateJournalEntry = async (id: string, content: string) => {
+  const user = await findUserByClerkId();
+  const updatedEntry = await prisma.entry.update({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id,
+      },
+    },
+    data: {
+      content,
+    },
+  });
+
+  return updatedEntry;
 };
