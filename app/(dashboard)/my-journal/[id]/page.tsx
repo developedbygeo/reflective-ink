5d600ce95ecbc4ce9bbedd177e6dc1d11d18bd8e
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-16 00:38:16 +0200

    chore: cleanup

--- app/(dashboard)/my-journal/[id]/page.tsx
+++ app/(dashboard)/my-journal/[id]/page.tsx
@@ -1,3 +1,5 @@
+import SentimentFigure from '@/components/UI/SentimentFigure';
+import Analysis from '@/components/modules/myJournal/Analysis';
 import Editor from '@/components/modules/myJournal/Editor';
 import { getJournalEntry } from '@/utils/prisma-utils';
 
@@ -8,23 +10,12 @@ type EntryPageParams = {
 const EntryPage = async ({ params }: EntryPageParams) => {
   const { id } = params;
   const entry = await getJournalEntry(id);
-  const analysisData = [
-    { name: 'Summary', value: 'Current summary' },
-    { name: 'Subject', value: 'Current subject' },
-    { name: 'Mood', value: 'Custom mood' },
-    { name: 'Negative', value: false },
-  ];
 
   return (
-    <div className="w-full grid grid-cols-12 mt-32 gap-4 h-full">
-      <section className="col-span-6 col-start-2">
-        <h1 className="">Entry {id}</h1>
-        <Editor entry={entry} />
-      </section>
-      <section className="border-l col-start-10 col-span-3 border-black/10">
-        <div className="bg-blue-300 h-full px-6 py-1">
-          <h2 className="text-2xl">Analysis</h2>
-        </div>
+    <div className="w-full mt-32 h-full">
+      <section className="">
+        <h1 className="col-start-2 col-span-8">Entry {id}</h1>
+        <Editor className="" entry={entry} />
       </section>
     </div>
   );
