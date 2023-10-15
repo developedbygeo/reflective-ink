c12e54272bb224a6450e5984902cf3aa57912b78
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-16 00:39:32 +0200

    chore: adding data types

---
+++ types/data.ts
@@ -0,0 +1,35 @@
+import { Entry } from '@prisma/client';
+
+export type EntryAnalysis = {
+  summary?: string;
+  subject?: string;
+  mood?: string;
+  negative?: string;
+};
+
+export type EntryAnalysisData = {
+  name: string;
+  value: string | boolean | undefined;
+};
+
+export type AugmentedEntry =
+  | (Entry & {
+      analysis: {
+        id: string;
+        createdAt: Date;
+        updatedAt: Date;
+        entryId: string;
+        mood: string;
+        summary: string;
+        subject: string;
+        color: string;
+        negative: boolean;
+      } | null;
+    } & {
+      id: string;
+      createdAt: Date;
+      updatedAt: Date;
+      userId: string;
+      content: string;
+    })
+  | null;
