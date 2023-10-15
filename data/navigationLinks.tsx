a27c712fc17368fd73997c32198aacc0cfb01e3e
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-16 00:39:37 +0200

    chore: adding links

---
+++ data/navigationLinks.tsx
@@ -0,0 +1,8 @@
+import { LuHome, LuBook } from 'react-icons/lu';
+
+const navigationLinks = [
+  { name: 'Home', href: '/', icon: LuHome },
+  { name: 'My Journal', href: '/my-journal', icon: LuBook },
+];
+
+export default navigationLinks;
