7b40b86a3357d6bfa9516bb49d03fc335877193d
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-15 20:13:46 +0200

    fix: patch now upserts analysis

--- app/api/my-journal/[id]/route.ts
+++ app/api/my-journal/[id]/route.ts
@@ -1,7 +1,7 @@
-import { findUserByClerkId } from '@/utils/auth';
-import { prisma } from '@/utils/db';
-import { updateJournalEntry } from '@/utils/prisma-utils';
 import { NextRequest, NextResponse } from 'next/server';
+import { prisma } from '@/utils/db';
+import { analyze } from '@/utils/ai';
+import { updateJournalEntry } from '@/utils/prisma-utils';
 
 export const PATCH = async (
   request: NextRequest,
@@ -10,6 +10,18 @@ export const PATCH = async (
   const data = await request.json();
 
   const updatedEntry = await updateJournalEntry(params.id, data.content);
+  const analysis = await analyze(updatedEntry.content);
+
+  if (analysis) {
+    await prisma.analysis.upsert({
+      where: { entryId: updatedEntry.id },
+      create: {
+        entryId: updatedEntry.id,
+        ...analysis,
+      },
+      update: analysis,
+    });
+  }
 
   return NextResponse.json({ data: updatedEntry });
 };
