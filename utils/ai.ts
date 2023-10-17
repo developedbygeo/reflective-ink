13c5e0cd0e7f1e5ca1296f388f3093dcc05d72b6
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-18 01:04:19 +0200

    feat: qaModel helper

--- utils/ai.ts
+++ utils/ai.ts
@@ -1,7 +1,13 @@
-import { OpenAI } from 'langchain/llms/openai';
-import { StructuredOutputParser } from 'langchain/output_parsers';
-import { PromptTemplate } from 'langchain/prompts';
 import z from 'zod';
+import { OpenAI } from 'langchain/llms/openai';
+import { PromptTemplate } from 'langchain/prompts';
+import { StructuredOutputParser } from 'langchain/output_parsers';
+import { Document } from 'langchain/document';
+import { loadQARefineChain } from 'langchain/chains';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { MemoryVectorStore } from 'langchain/vectorstores/memory';
+
+import { AugmentedEntry, QaEntry } from '@/types/data';
 
 const promptParser = StructuredOutputParser.fromZodSchema(
   z.object({
@@ -52,4 +58,43 @@ export const analyze = async (content: string) => {
   } catch (err) {
     console.log(err);
   }
+};
+
+// a qaModal which takes in a question and a list of entries, creates a vector db in memory and returns the answer to the question (by grouping the entries into relevant and irrelevant entries)
+
+export const qaModel = async (question: string, entries: QaEntry[]) => {
+  const documents = entries.map(
+    (entry) =>
+      new Document({
+        pageContent: entry.content,
+        metadata: { source: entry.id, date: entry.createdAt },
+      }),
+  );
+
+  const model = new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' });
+
+  // https://js.langchain.com/docs/modules/chains/document/refine
+  // https://js.langchain.com/docs/modules/chains/document/refine#refine
+  const qaRefineChain = loadQARefineChain(model);
+
+  // https://js.langchain.com/docs/api/embeddings_openai/classes/OpenAIEmbeddings
+  // embeddings are basically a group of vectors that represent some text.
+  // we are essentially using the hosted embeddings from OpenAI
+  const embeddings = new OpenAIEmbeddings();
+  const vectorStore = await MemoryVectorStore.fromDocuments(
+    documents,
+    embeddings,
+  );
+
+  // relevantDocs - based on the question, we know what entries we need to answer it.
+  const relevantDocs = await vectorStore.similaritySearch(question);
+
+  const response = await qaRefineChain.call({
+    input_documents: relevantDocs,
+    question,
+    instructions:
+      'Please provide your answer in a friendly, empathic manner with a conversational tone.',
+  });
+
+  return response.output_text;
 };
