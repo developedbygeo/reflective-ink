e4ce386d2c03b43bc8cd458bf82f2d04598b6a2d
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-19 02:15:28 +0200

    feat: adding my-history page

---
+++ app/(dashboard)/my-history/page.tsx
@@ -0,0 +1,30 @@
+import MoodChart from '@/components/modules/myHistory/MoodChart';
+import { getHistoryData } from '@/utils/prisma-utils';
+
+const History = async () => {
+  const { analysisAvg, analysisTotal } = await getHistoryData();
+  return (
+    <section className="w-full h-full container mt-20">
+      <article>
+        <h1 className="text-xl text-gray-200">
+          Here is an line chart of your mood on a daily basis, based on your
+          entries.
+        </h1>
+        <div className="w-3/5 mt-4 text-base text-gray-300">
+          <p>
+            The Average Sentiment (AS) is calculated by taking the average of
+            all the sentiment scores of your entries. Each entry is given a
+            score between -10 and 10, with -10 being the most negative and 10
+            being the most positive.
+          </p>
+          <p className="mt-2">Your AS is: {analysisAvg}</p>
+        </div>
+      </article>
+      <div className="w-full h-[50vh]">
+        <MoodChart data={analysisTotal} />
+      </div>
+    </section>
+  );
+};
+
+export default History;
