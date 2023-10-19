4c70a3cf72d7006787c2baf7e73bd22988aeca5c
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-20 01:36:53 +0200

    chore: disabling theme toggle

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -8,7 +8,7 @@ const Layout = ({ children }: WithChildren) => (
   <div className="min-h-[100vh] overflow-y-auto relative bg-darkGray">
     <header className="h-16 border-b flex items-center justify-end px-8 py-4 border-darkGray">
       <div className="flex gap-6">
-        <ThemeToggle />
+        {/* <ThemeToggle /> */}
         <UserButton appearance={userButtonStyle} />
       </div>
     </header>
