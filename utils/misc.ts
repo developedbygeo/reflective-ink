16faead2068bd2e341895ced2ff9f2b508818c1b
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-19 02:18:12 +0200

    chore: helper function for parsing xaxis date

--- utils/misc.ts
+++ utils/misc.ts
@@ -1,2 +1,11 @@
 export const generateNumberInRange = (min: number, max: number) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
+
+export const parseDateForTable = (date: Date) => {
+  return date.toLocaleDateString('en-US', {
+    weekday: 'short',
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+  });
+};
