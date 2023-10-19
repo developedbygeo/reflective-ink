e74d95db3d5b3051bbec8de708802020caeaccdc
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-19 02:17:28 +0200

    chore: adding my-history link

--- data/navigationLinks.tsx
+++ data/navigationLinks.tsx
@@ -1,8 +1,9 @@
-import { LuHome, LuBook } from 'react-icons/lu';
+import { LuHome, LuBook, LuCalendarDays } from 'react-icons/lu';
 
 const navigationLinks = [
   { name: 'Home', href: '/', icon: LuHome },
   { name: 'My Journal', href: '/my-journal', icon: LuBook },
+  { name: 'My History', href: '/my-history', icon: LuCalendarDays },
 ];
 
 export default navigationLinks;
