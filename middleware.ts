44980bad77eb0b7164502fa4ce711fd6f460204d
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-20 01:36:13 +0200

    chore: formatting

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
-import { authMiddleware } from '@clerk/nextjs'
+import { authMiddleware } from '@clerk/nextjs';
 
 // middleware checks cookies and redirects to login if not logged in
 export default authMiddleware({
   publicRoutes: ['/'],
-})
+});
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-}
+};
