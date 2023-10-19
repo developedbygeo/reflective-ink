0f29d24ef0c2681218ea618dc4ed6074c1997bfd
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-20 01:36:44 +0200

    style: adding homepage responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
-import Link from 'next/link'
-import { auth } from '@clerk/nextjs'
+import Link from 'next/link';
+import { auth } from '@clerk/nextjs';
 
-import { Button } from '@/components/UI/Button'
-import MovingGradient from '@/components/UI/MovingGradient'
+import { Button } from '@/components/UI/Button';
+import MovingGradient from '@/components/UI/MovingGradient';
 
 const Homepage = () => {
-  const { userId } = auth()
-  const href = userId ? '/my-journal' : '/new-user'
-  const linkText = userId ? 'My Journal' : 'Sign up'
+  const { userId } = auth();
+  const href = userId ? '/my-journal' : '/new-user';
+  const linkText = userId ? 'My Journal' : 'Sign up';
 
   return (
     <div className="h-screen bg-gray-100 dark:bg-darkGray flex justify-center items-center text-white relative">
       <MovingGradient className="pointer-events-none" />
-      <article className="w-full max-w-[40rem] mx-auto">
+      <article className="w-full container">
         <h1 className="text-4xl w-2/3 mb-4">Welcome to Reflective Ink.</h1>
         <p className="text-xl font-light h-4/5 text-gray-300 mb-6">
           An AI-powered app for tracking your mood. Interested in learning more?
@@ -22,7 +22,7 @@ const Homepage = () => {
         </Button>
       </article>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
